83094cfa026e609cee7f9256645b6a0edfeadf4c
Nathália Arantes <49135777+nathaliarantes@users.noreply.github.com>
2020-05-31 20:46:27 -0300

    Update visualizar.php

--- application/views/projeto/visualizar.php
+++ application/views/projeto/visualizar.php
@@ -40,12 +40,8 @@
 										      <div class="modal-body">
 											<div style="border: 2px solid #e3e7f0; border-radius: 10px; padding: 30px 20px; margin: 0px 20px;">
 												<form action="<?=site_url('projeto/finalizar/'.$projeto['id_projeto'])?>" method="POST">
-													<div class="row">
-														<div class="col-md-12">
-															<div id="usuariosProjeto">
+													<div id="usuariosProjeto">
 
-															</div>
-														</div>
 													</div>
 													<button class="btn btn-primary btn-block btn-lg">Finalizar</button>
 												</form>
